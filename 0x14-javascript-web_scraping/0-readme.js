e94e2e883138f31c2aef19a4ff27bb649cccb80e
Mehdi_AI <mehdiaititto239@gmail.com>
2024-03-26 23:32:31 +0000

    'update'

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,12 +1,5 @@
 #!/usr/bin/node
 const fs = require('fs');
-
 fs.readFile(process.argv[2], 'utf8', function (error, content) {
-  if (error) {
-    console.error(error);
-  } else if (content === null || content === undefined) {
-    console.error("Empty content or unable to read the file.");
-  } else {
-    console.log(content);
-  }
+  console.log(error || content);
 });
